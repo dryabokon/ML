ddecfe8ee700ee594ca7725665f2f63ea2b4b321
Dima <d.ryabokon@gmail.com>
2019-07-15 09:23:36 +0300

    mAP OVP

---
+++ ex_02_01_train_test_2_classes_multi_dim.py
@@ -0,0 +1,48 @@
+import os
+import matplotlib.pyplot as plt
+# ----------------------------------------------------------------------------------------------------------------------
+import tools_ML
+import tools_IO
+# ----------------------------------------------------------------------------------------------------------------------
+import classifier_Bayes
+import classifier_SVM
+import classifier_RF
+import classifier_Ada
+import classifier_Gauss
+import classifier_LM
+import classifier_KNN
+# ---------------------------------------------------------------------------------------------------------------------
+import generator_Bay
+import generator_Gauss
+# ---------------------------------------------------------------------------------------------------------------------
+from ex_02_01_train_test_2_classes import generate_data_syntetic
+# ---------------------------------------------------------------------------------------------------------------------
+def E2E_features_2_classes_multi_dim(filename_data_pos,filename_data_neg,folder_out):
+
+    filename_scrs_pos = folder_out+'scores_pos.txt'
+    filename_scrs_neg = folder_out+'scores_neg.txt'
+
+    #Classifier = classifier_Bayes.classifier_Bayes()
+    #Classifier = classifier_KNN.classifier_KNN()
+    #Classifier = classifier_Ada.classifier_Ada()
+    Classifier = classifier_SVM.classifier_SVM()
+    #Classifier = classifier_RF.classifier_RF()
+    #Classifier = classifier_LM.classifier_LM()
+    #Classifier = classifier_Gauss.classifier_Gauss()
+
+    ML = tools_ML.tools_ML(Classifier)
+    ML.E2E_features_2_classes_multi_dim(folder_out, filename_data_pos, filename_data_neg)
+
+    return
+# ----------------------------------------------------------------------------------------------------------------------
+if __name__ == '__main__':
+
+
+    folder_in = 'data/ex_features_LPR/CNN_AlexNet_TF/'
+
+    folder_out = 'data/output/'
+    #generate_data_syntetic(folder_out,5)
+
+    E2E_features_2_classes_multi_dim(folder_in+'pos.txt', folder_in+'neg.txt',folder_out)
+
+
