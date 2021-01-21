50930322d6ac54d408f4d2217d2721185f9261c9
dryabokon <d.ryabokon@gmail.com>
2021-01-21 19:56:00 +0200

    titanic

--- ex_06_titanic.py
+++ ex_06_titanic.py
@@ -11,12 +11,12 @@ if __name__ == '__main__':
     df['who'] = df['who'].map({'man': 0, 'woman': 1, 'child': 2})
     df['alone'] = df['alone'].map({True: 1, False: 0})
 
-    X = df[['age', 'pclass', 'sex', 'sibsp', 'parch', 'embarked', 'who', 'alone']].to_numpy()
+    X = df[['age', 'pclass', 'sex', 'sibsp', 'parch', 'embarked', 'who', 'alone']]
     Y = df[['survived']].to_numpy()
 
-    f_scores = chi2(df, Y)[1]
+    f_scores = chi2(X, Y)[1]
     idx =  numpy.argsort(-f_scores)
 
     print('f_score\tfeature_name')
-    for feature_name,f_score in zip(df.columns.to_numpy()[idx], f_scores[idx]):
+    for feature_name,f_score in zip(X.columns.to_numpy()[idx], f_scores[idx]):
         print('%1.2f\t%s'%(f_score,feature_name))
