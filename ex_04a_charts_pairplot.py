b51c3d751b13fa9d9c1b84cc43d0e2cbfb402f03
dryabokon <d.ryabokon@gmail.com>
2022-02-01 14:48:38 +0200

    charts

--- ex_04a_charts_pairplot.py
+++ ex_04a_charts_pairplot.py
@@ -5,11 +5,13 @@ import matplotlib.pyplot as plt
 # ----------------------------------------------------------------------------------------------------------------------
 import tools_plot_v2
 import tools_IO
+import tools_Hyptest
 # ----------------------------------------------------------------------------------------------------------------------
 folder_in = './data/ex_datasets/'
 folder_out = './data/output/'
 # ----------------------------------------------------------------------------------------------------------------------
-P = tools_plot_v2.Plotter(folder_out,dark_mode=False)
+P = tools_plot_v2.Plotter(folder_out,dark_mode=True)
+HT = tools_Hyptest.HypTest()
 # ----------------------------------------------------------------------------------------------------------------------
 def ex_heart():
     df = pd.read_csv(folder_in + 'dataset_heart.csv', delimiter=',')
@@ -23,26 +25,25 @@ def ex_houses():
 # ----------------------------------------------------------------------------------------------------------------------
 def ex_titatic():
     df = seaborn.load_dataset('titanic')
+    #df.to_csv(folder_out + 'titanic.csv', index=False)
+    #df = tools_DF.impute_na(df,strategy='mean')
+    df = df.drop(columns=['alive'])
 
     # survived
-    idx_target = 0
-    palette = 'tab10'
+    idx_target = df.columns.get_loc('survived')
 
+    df_Q = HT.distribution_distances(df, idx_target)
+    df_Q.to_csv(folder_out + 'df_Q.csv')
+    th = numpy.quantile(df_Q.values, 0.90)
+    df_Q = (df_Q >= th)
 
-    #
-    # idx_target = 3
-    # target_min = df.iloc[:, idx_target].min()
-    # target_max = df.iloc[:, idx_target].max()
-    # N = 10
-    # bins = numpy.linspace(target_min, target_max, N)
-    # XX = pd.cut(df.iloc[:, idx_target], bins=bins,labels=bins[:-1].astype(int)).values.to_numpy()
-    # df.iloc[:, idx_target] = XX
-    # palette = plt.get_cmap('jet')
-    # P.pairplots_df(df, idx_target,add_noise=True, palette=palette,mode2d=True)
+    # sex
+    # idx_target = df.columns.get_loc('sex')
+    # df['sex'] = df['sex'].map({'male':0,'female':1})
 
-    P.jointplots_df(df, idx_target)
-    P.pairplots_df(df, idx_target, cumul_mode=True)
-    #P.pairplots_df(df, idx_target, add_noise=False,palette=palette)
+    #P.histoplots_df(df, idx_target)
+    P.pairplots_df(df, idx_target, df_Q, add_noise=True,remove_legend=False)
+    #P.pairplots_df(df, idx_target,df_Q, cumul_mode=True)
 
     return
 # ----------------------------------------------------------------------------------------------------------------------
